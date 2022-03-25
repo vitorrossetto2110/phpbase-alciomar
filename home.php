b1ac68367db50c18513e69811ba963947b90e869
vitorrossetto2110 <98957321+vitorrossetto2110@users.noreply.github.com>
2022-03-25 09:53:52 -0300

    registro

--- home.php
+++ home.php
@@ -53,5 +53,6 @@ echo $f['image'];?>" width="100px" height="100px">
 <a href="edit.php">Edit</a>
 <a href="delete.php">Delete</a>
 <a href="logout.php">Logout</a>
+<a href="reg_city.php">city</a>
     </body>
 </html>
--- reg_city.php
+++ reg_city.php
@@ -1,16 +1,12 @@
 <?php
 include 'connect.php';
+include 'CheckLogin.php';
 if(isset($_POST['sub'])){
-    $t=$_POST['text'];
-    $u=$_POST['user'];
-    $p=$_POST['pass'];
-    $c=$_POST['city'];
-    $g=$_POST['gen'];
-    if($_FILES['f1']['name']){
-    move_uploaded_file($_FILES['f1']['tmp_name'], "image/".$_FILES['f1']['name']);
-    $img="image/".$_FILES['f1']['name'];
-    }
-    $i="insert into reg(name,username,password,city,image,gender)value('$t','$u','$p','$c','$img','$g')";
+    $ID_city=$_POST['text'];
+    
+    
+   //$i="insert into reg(name,username,password,city,image,gender)value('$t','$u','$p','$c','$img','$g')";
+   $i = "insert into city(city) values ('$ID_city')";
     mysqli_query($con, $i);
 }
 ?>
@@ -29,50 +25,9 @@ if(isset($_POST['sub'])){
                         <input type="text" name="text">
                     </td>
                 </tr>
-                <tr>
-                    <td>
-                        Username
-                        <input type="text" name="user">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        password
-                        <input type="password" name="pass">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        city
-                        <select name="city">
-                            <option value="">-select-</option>
-                            <?php
-                            $sqlCity = mysqli_query($con, "select * from city");
-
-                            while($item = mysqli_fetch_assoc($sqlCity))
-                            {
-                                $nomeItem = $item["city"];
-                                $idCity = $item["ID_city"];
-                                echo"
-                                <option value=$nomeItem>$nomeItem</option>
-                                ";
-                            }
-                            ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Gender
-                        <input type="radio"name="gen" id="gen" value="male">male
-                        <input type="radio" name="gen" id="gen" value="female">female
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Image
-                        <input type="file" name="f1">
-                    </td>
-                </tr>
+                
+                    
+                 
                 <tr>
                     <td>
                         <input type="submit" value="submit" name="sub">        
